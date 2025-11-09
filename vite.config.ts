bedeebedd45bcf4a34f5edccd53d68a77412ebe2
Pawan Kumar <pawankumar9aa@gmail.com>
2025-11-09 19:41:37 +0530

    a

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,8 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
-  ],
-   base: '/VCportfolio/',
+    ],
+   
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
